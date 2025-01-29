3fdc1a690d9d86d6fcb6e847e9ba6dcf17477266
KhaledMohammed022 <121465866+KhaledMohammed022@users.noreply.github.com>
2025-01-29 21:31:26 +0530

    Update question1.js

--- question1.js
+++ question1.js
@@ -1,4 +1,4 @@
-// The issue in your code arises because the setTimeout function is asynchronous. By the time the callback function inside setTimeout is executed,
+// The issue in code arises because the setTimeout function is asynchronous. By the time the callback function inside setTimeout is executed,
 // the loop has already finished running, and the value of i is 5 for all iterations.
 // This happens because var is function-scoped, not block-scoped, so it doesn’t create a new scope for each iteration.
 for (let i = 0; i < 5; i++) {
